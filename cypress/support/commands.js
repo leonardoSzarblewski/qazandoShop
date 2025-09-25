f1c49872fd4a44f24c55c04a5b1c2ae55d13e478
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-09-25 14:38:19 -0300

    comando login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,12 @@
-Cypress.Commands.add('preencheCadastro', (nome, email, senha) => {
+Cypress.Commands.add('cadastro', (nome, email, senha) => {
     cy.get('#user').type(nome)
     cy.get('#email').type(email)
     cy.get('#password').type(senha)
     cy.get('#btnRegister').click()
+})
 
+Cypress.Commands.add('login', (email, senha) => {
+    cy.get('#user').type(email)
+    cy.get('#password').type(senha)
+    cy.get('#btnLogin').click()
 })
